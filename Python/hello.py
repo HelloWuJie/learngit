46782e4b1432444f433838e9ed20fabf42e9bd1a
HelloWuJie <731362276@qq.com>
2020-03-10 17:31:46 +0800

    20200310

---
+++ Python/hello.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+'a test moudle'
+__author__ = 'jie wu'
+import sys
+
+
+def test():
+    args = sys.argv
+    if len(args) == 1:
+        print('hello, world!')
+    elif len(args) == 2:
+        print('hello, %s' % args[1])
+    else:
+        print('too many arguments!')
+
+
+if __name__ == '__main__':
+    test()
+
+
