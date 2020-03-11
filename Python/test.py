8e8e67a3e6b116361cb3ffc63ee7e989627051b4
HelloWuJie <731362276@qq.com>
2020-03-11 17:32:16 +0800

    20200311

--- Python/test.py
+++ Python/test.py
@@ -338,4 +338,55 @@ def fast(x, y):
     return x + y
 f = fast(11, 22)
 print(f)
+
+
+
+class Student(object):
+    def __init__(self, name, score):
+        self.__name = name
+        self.__score = score
+
+    def print_score(self):
+        print('%s:%s' % (self.__name, self.__score))
+
+    def get_name(self):
+        return self.__name
+    
+    def get_score(self):
+        return self.__score
+    
+    def set_score(self, score):
+        if 0 <= score <= 100:
+            self.__score = score
+        else:
+            raise ValueError('bad score')
+    
+bart = Student('Bart', 99)
+print(bart.get_name(),':',bart.get_score())
+bart.set_score(101)
+print(bart.get_name(),':',bart.get_score())
+
+#bart.print_score()
+
+
+#请把下面的Student对象的gender字段对外隐藏起来，用get_gender()和set_gender()代替，并检查参数有效性：
+class Student(object):
+    def __init__(self, name, gender):
+        self.name = name
+        self.__gender = gender
+
+    def get_gender(self):
+        return self.__gender
+
+    def set_gender(self, gender):
+        if gender in ('male','female'):
+            self.__gender = gender
+        else:
+            raise ValueError('bad gender')
+
+bart = Student('Bart Wu','male')
+print(bart.name,bart.get_gender())
+bart.set_gender('female')
+print(bart.name,bart.get_gender())
+
 '''
